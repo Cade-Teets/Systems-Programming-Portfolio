c5447d32a70dbb0340fcd6a80844c4d0d5366266
Chloe <cf2024@uw.edu>
2025-07-09 15:44:34 -0700

    updated hw3 starter files, including ones for testing purposes

---
+++ hw3/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This is my first C program!
+ */
+
+// This prints out "Hello World!"
+int main(int argc, char* argv[]) {
+    printf("Hello World!\n");
+    return 0;
+}
--- hw3/wordcount.c
+++ hw3/wordcount.c
@@ -1,14 +1,30 @@
-/*
-    TODO: Include basic information.
+/**
+    TODO: Fill in the following basic information.
+
+    Author: [First and Last Name]
+    UWNetID: [Your UWNetID]
+    CSE 374 25su Homework 3: Wordcount
+    Date: [mm/dd/yyyy]
+
+    Citations: [List external resources or people you consulted]
+
+    Description: [Brief description of the purpose of this program]
 */
 
 // TODO: Includes for functions & types defined elsewhere.
+#include <stdio.h>
+#include <stdlib.h>
 
 // TODO: Defines for symbolic constants (if any).
 
 // TODO: Function declarations/prototypes (with appropriate comments).
 
 // TODO: Function definitions.
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+
+    // TODO: Your implementation here
+
+    exit(EXIT_SUCCESS);
+}
 
 // TODO: Remove all TODO comments (like this one) once you are done!
