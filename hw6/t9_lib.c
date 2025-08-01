3586a713962b3fab8ffd88dd6f447cc2dbebba70
Chloe <cf2024@uw.edu>
2025-08-01 00:51:06 -0700

    adding starter files for hw6

---
+++ hw6/t9_lib.c
@@ -0,0 +1,48 @@
+/**
+    TODO: Fill in the following basic information.
+
+    Author: [First and Last Name]
+    UWNetID: [Your UWNetID]
+    CSE 374 25su Homework 6: T9 Implementation
+    Date: [mm/dd/yyyy]
+
+    Citations: [List external resources or people you consulted]
+
+    Description: [Brief description of the purpose of this program]
+*/
+
+// TODO: Includes for functions & types defined elsewhere.
+#include <stddef.h>
+
+#include "t9_lib.h"
+#include "t9_priv.h"
+
+// TODO: Defines for symbolic constants (if any).
+
+// TODO: Function declarations/prototypes (with appropriate comments).
+
+
+T9* InitializeEmptyT9() {
+    // TODO: your code goes here
+    return NULL;  // you will want to change this
+}
+
+T9* InitializeFromFileT9(const char* filename) {
+    // TODO: your code goes here
+    return NULL;  // you will want to change this
+}
+
+void AddWordToT9(T9* dict, const char* word) {
+    // TODO: your code goes here
+}
+
+char* PredictT9(T9* dict, const char* nums) {
+    // TODO: your code goes here
+    return NULL;  // you will want to change this
+}
+
+void DestroyT9(T9* dict) {
+    // TODO: your code goes here
+}
+
+// TODO: Remove all TODO comments (like this one) once you are done!
