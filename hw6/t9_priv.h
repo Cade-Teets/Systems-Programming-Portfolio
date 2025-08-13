db91b32a9630e3bded8ba7dce592982b30006a8a
cteets <cteets@calgary.cs.washington.edu>
2025-08-12 23:00:48 -0700

    Fixed header file whitespace issue

--- hw6/t9_priv.h
+++ hw6/t9_priv.h
@@ -3,7 +3,7 @@
 
 typedef struct T9 {
     // Pointer to node for each number on the T9 Keypad
-    // i = 0 -> 2, ..., i 
+    // i = 0 -> 2, ..., i
     T9* node[8];
     char** val;
     int list_count;
