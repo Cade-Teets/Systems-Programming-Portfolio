edbfd54f36bbf2879670136f999dadcadd2e4bd8
cteets <cteets@calgary.cs.washington.edu>
2025-08-17 12:03:02 -0700

    Fixed inconsistent spacing.

--- hw7/Vector.cc
+++ hw7/Vector.cc
@@ -36,7 +36,7 @@ namespace vector374 {
     }
     float Vector::length() const {
         return std::sqrt(v_[0] * v_[0]
-          + v_[1] * v_[1] + v_[2] * v_[2]);
+            + v_[1] * v_[1] + v_[2] * v_[2]);
     }
     // Member overloaded operators
     Vector& Vector::operator=(const Vector& v) {
@@ -65,34 +65,34 @@ namespace vector374 {
     // Nonmember overloaded operators
     Vector operator+(const Vector& v1, const Vector& v2) {
         return Vector(v1.get_X() + v2.get_X(),
-          v1.get_Y() + v2.get_Y(), v1.get_Z() + v2.get_Z());
+            v1.get_Y() + v2.get_Y(), v1.get_Z() + v2.get_Z());
     }
     Vector operator-(const Vector& v1, const Vector& v2) {
         return Vector(v1.get_X() - v2.get_X(),
-          v1.get_Y() - v2.get_Y(), v1.get_Z() - v2.get_Z());
+            v1.get_Y() - v2.get_Y(), v1.get_Z() - v2.get_Z());
     }
     float operator*(const Vector& v1, const Vector& v2) {
-        float temp = v1.get_X() * v2.get_X() +
-          v1.get_Y() * v2.get_Y() + v1.get_Z() * v2.get_Z();
+        float temp = v1.get_X() * v2.get_X() + v1.get_Y()
+            * v2.get_Y() + v1.get_Z() * v2.get_Z();
         return temp;
     }
     Vector operator*(const Vector& v, const float c) {
         return Vector(c * v.get_X(),
-          c * v.get_Y(), c * v.get_Z());
+            c * v.get_Y(), c * v.get_Z());
     }
     Vector operator*(const float c, const Vector& v) {
         return Vector(c * v.get_X(),
-          c * v.get_Y(), c * v.get_Z());
+            c * v.get_Y(), c * v.get_Z());
     }
     bool operator==(const Vector& v1, const Vector& v2) {
         return (v1.get_X() == v2.get_X() &&
-          v1.get_Y() == v2.get_Y() &&
-          v1.get_Z() == v2.get_Z());
+            v1.get_Y() == v2.get_Y() &&
+            v1.get_Z() == v2.get_Z());
     }
     bool operator!=(const Vector& v1, const Vector& v2) {
         return (v1.get_X() != v2.get_X() ||
-          v1.get_Y() != v2.get_Y() ||
-          v1.get_Z() != v2.get_Z());
+            v1.get_Y() != v2.get_Y() ||
+            v1.get_Z() != v2.get_Z());
     }
     std::ostream& operator<<(std::ostream& out, const Vector& v) {
         out << "(" << v.get_X() << "," << v.get_Y() << "," << v.get_Z() << ")";
